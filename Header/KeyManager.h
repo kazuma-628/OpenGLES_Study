103abb94aaab453d6dd7344d12bfc5863633856c
Kazuma Shibata <kazuma-628@users.noreply.github.com>
2016-02-27 05:15:22 +0900

    キー管理クラスを作成、中身はまだ未実装だがMain関数で既にコールしてしまっているので暫定でコミット

---
+++ Header/KeyManager.h
@@ -0,0 +1,31 @@
+#ifndef KEYMANAGER_H
+#define KEYMANAGER_H
+
+//include定義
+#include "Common.h"
+
+class KeyManager
+{
+
+public:
+	//コンストラクタ
+	KeyManager();
+
+	//デストラクタ
+	~KeyManager();
+
+	/*-------------------------------------------------------------------------------
+	*	関数説明
+	*	　Key管理マネージャーを初期化する
+	*	引数
+	*	　なし
+	*	戻り値
+	*	　なし
+	*-------------------------------------------------------------------------------*/
+	void Initialize();
+
+
+private:
+
+};
+#endif
