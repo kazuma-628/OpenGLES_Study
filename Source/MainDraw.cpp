cf9e32bc21a441d514f52450caf0ff560fe09145
Kazuma Shibata <kazuma-628@users.noreply.github.com>
2016-02-26 23:54:47 +0900

    3D空間が分かりやすいようにサンプルプログラムを変更

--- Source/MainDraw.cpp
+++ Source/MainDraw.cpp
@@ -82,23 +82,31 @@ void MainDraw::Draw(GLFWwindow *const p_window)
 //	printf("x_pos = %lf x_pos2 = %lf y_pos = %lf y_pos2 = %lf\n", x_pos, x_pos2, y_pos, y_pos2);
 
 	//カメラの映る位置に移動させ、マウスでも移動できるようにする。
-	ModelView.Translate(x_pos2, y_pos2, -20.0);
+	ModelView.Translate(0.0, 0.0, -30.0);
 	
+	ModelView.Rotate(x_pos2 * 10, 0.0, 1.0, 0.0);
+
+	ModelView.Rotate(y_pos2 * 10, 1.0, 0.0, 0.0);
+
+	ModelView.Rotate(-90, 0.0, 1.0, 0.0);
+
+	/////////////////////////////
+	// デバッグ用の自動回転
+
 	//回転させる
-	ModelView.Rotate(1000 * move_y, 0.0, 1.0, 0.0);
-	
+//	ModelView.Rotate(1000 * move_y, 0.0, 1.0, 0.0);
 	//移動量を増加させる。
 //	move_x = move_x + 0.001f;
-	move_y = move_y + 0.001f;
+//	move_y = move_y + 0.001f;
 
 	//値をリセットする（動作確認用の一時的なプログラム）
-	if (move_x > 10 || move_y > 10 )
-	{
-		move_x = move_y = 0;
-	}
+//	if (move_x > 10 || move_y > 10 )
+//	{
+//		move_x = move_y = 0;
+//	}
 
 	//透視投影行列を適用する
-	Projection.Perspective(-10.0, 10.0, -10.0, 10.0, 10.0, 30.0);
+	Projection.Perspective(-10.0, 10.0, -10.0, 10.0, 10.0, 50.0);
 
 	//シェーダーの変数を有効化
 	glEnableVertexAttribArray(m_attr_pos);
@@ -108,34 +116,87 @@ void MainDraw::Draw(GLFWwindow *const p_window)
 	// 画面中央へ描画する
 	const GLfloat position[] =
 	{
-		// v0
-		-10.0f, 10.0f, 0.0f,
 		// v1
-		10.0f, 10.0f, 0.0f,
+		-10.0f, 10.0f, 10.0f,
 		// v2
-		10.0f, -10.0f, 0.0f,
+		10.0f, 10.0f, 10.0f,
 		// v3
-		-10.0f, -10.0f, 0.0f
+		-10.0f, -10.0f, 10.0f,
+		// v4
+		10.0f, -10.0f, 10.0f,
+
+		// v5
+		-10.0f, -10.0f, -10.0f,
+		// v6
+		10.0f, -10.0f, -10.0f,
+
+		// v7
+		-10.0f, 10.0f, -10.0f,
+		// v8
+		10.0f, 10.0f, -10.0f,
+
+		// v9
+		-10.0f, 10.0f, 10.0f,
+		// v10
+		10.0f, 10.0f, 10.0f,
+
+		// v11
+		10.0f, 10.0f, 10.0f,
+		// v12
+		10.0f, -10.0f, 10.0f,
+		// v13
+		10.0f, 10.0f, -10.0f,
+		// v14
+		10.0f, -10.0f, -10.0f,
+
+
 	};
 
 	// 頂点カラーを設定する
 	const GLubyte color[] =
 	{
-		// v0 rgb
-		200, 200, 200,
 		// v1 rgb
-		200, 0, 0,
+		50, 50, 50,
 		// v2 rgb
-		0, 200, 0,
+		90, 0, 0,
 		// v3 rgb
-		0, 0, 200
+		0, 90, 0,
+		// v4 rgb
+		0, 0, 90,
+
+		// v5 rgb
+		130, 0, 0,
+		// v6 rgb
+		0, 130, 0,
+
+		// v7 rgb
+		0, 0, 130,
+		// v8 rgb
+		170, 0, 0,
+
+		// v9 rgb
+		0, 170, 0,
+		// v10 rgb
+		0, 0, 170,
+
+		// v11 rgb
+		210, 0, 0,
+		// v12 rgb
+		0, 210, 0,
+		// v13 rgb
+		0, 0, 210,
+		// v14 rgb
+		250, 0, 0,
 	};
+
+	//震度テストを有効
+	glEnable(GL_DEPTH_TEST);
 
 	//背景色指定
 	glClearColor(0.0f, 0.0f, 0.9f, 1.0f);
 
 	//色初期化
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	//ビューポートを設定する
 	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -145,7 +206,7 @@ void MainDraw::Draw(GLFWwindow *const p_window)
 	glVertexAttribPointer(m_attr_color, 3, GL_UNSIGNED_BYTE, GL_TRUE, 0, color);
 	glUniformMatrix4fv(m_ModelView_matrix, 1, GL_FALSE, ModelView.GetMatrix());
 	glUniformMatrix4fv(m_Proj_matrix, 1, GL_FALSE, Projection.GetMatrix());
-	glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 14);
 
 	//描画処理
 	glfwSwapBuffers(p_window);
