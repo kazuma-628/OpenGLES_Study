5bf660ccc4f1f69cf7d816eded9549c360f1c433
Kazuma Shibata <kazuma-628@users.noreply.github.com>
2016-07-13 00:38:21 +0900

    ・ユティリティの関数群をポインタ渡しから参照渡しに変更

--- Source/Utility.cpp
+++ Source/Utility.cpp
@@ -21,21 +21,14 @@ Utility::~Utility()
 *	　正常終了		：ベクトルの長さ
 *	　以上終了		：0.0		
 *-------------------------------------------------------------------------------*/
-float Utility::Length(Vec3 *p_Vector)
+float Utility::Length(Vec3 &p_Vector)
 {
 	float length = 0.0f;	//ベクトルの長さ
-
-	//引数チェック
-	if (NULL == p_Vector)
-	{
-		ERROR_MESSAGE("引数エラー");
-		return length;
-	}
 	
 	//計算しやすいように代入（誤差を少なくするために[double]で計算）
-	double x = (double)(p_Vector->x);
-	double y = (double)(p_Vector->y);
-	double z = (double)(p_Vector->z);
+	double x = (double)(p_Vector.x);
+	double y = (double)(p_Vector.y);
+	double z = (double)(p_Vector.z);
 
 	//ベクトルの長さを求める
 	length = (float)sqrt(x * x + y * y + z * z);
@@ -52,21 +45,14 @@ float Utility::Length(Vec3 *p_Vector)
 *	　正常終了		：正規化されたベクトル
 *	　以上終了		：全要素[0.0]
 *-------------------------------------------------------------------------------*/
-Vec3 Utility::Normalize(Vec3 *p_Vector)
+Vec3 Utility::Normalize(Vec3 &p_Vector)
 {
 	Vec3 normalize = { 0.0f };		//正規化されたベクトル
 
-	//引数チェック
-	if (NULL == p_Vector)
-	{
-		ERROR_MESSAGE("引数エラー");
-		return normalize;
-	}
-
 	//計算しやすいように代入（誤差を少なくするために[double]で計算）
-	double x = (double)(p_Vector->x);
-	double y = (double)(p_Vector->y);
-	double z = (double)(p_Vector->z);
+	double x = (double)(p_Vector.x);
+	double y = (double)(p_Vector.y);
+	double z = (double)(p_Vector.z);
 
 	//ベクトルの長さを求める
 	double length = sqrt(x * x + y * y + z * z);
@@ -102,25 +88,18 @@ Vec3 Utility::Normalize(Vec3 *p_Vector)
 *	　正常終了		：内積
 *	　以上終了		：0.0
 *-------------------------------------------------------------------------------*/
-float Utility::Dot(Vec3 *p_Vector1, Vec3 *p_Vector2)
+float Utility::Dot(Vec3 &p_Vector1, Vec3 &p_Vector2)
 {
 	float dot = 0.0f; //内積
 
-	//引数チェック
-	if (NULL == p_Vector1 || NULL == p_Vector2)
-	{
-		ERROR_MESSAGE("引数エラー");
-		return dot;
-	}
-
 	//計算しやすいように代入（誤差を少なくするために[double]で計算）
-	double x1 = (double)(p_Vector1->x);
-	double y1 = (double)(p_Vector1->y);
-	double z1 = (double)(p_Vector1->z);
+	double x1 = (double)(p_Vector1.x);
+	double y1 = (double)(p_Vector1.y);
+	double z1 = (double)(p_Vector1.z);
 
-	double x2 = (double)(p_Vector2->x);
-	double y2 = (double)(p_Vector2->y);
-	double z2 = (double)(p_Vector2->z);
+	double x2 = (double)(p_Vector2.x);
+	double y2 = (double)(p_Vector2.y);
+	double z2 = (double)(p_Vector2.z);
 
 	//内積を求める
 	dot = (float)((x1 * x2) + (y1 * y2) + (z1 * z2));
@@ -138,25 +117,18 @@ float Utility::Dot(Vec3 *p_Vector1, Vec3 *p_Vector2)
 *	　正常終了		：正規化されたベクトル
 *	　以上終了		：全要素[0.0]
 *-------------------------------------------------------------------------------*/
-Vec3 Utility::Cross(Vec3 *p_Vector1, Vec3 *p_Vector2)
+Vec3 Utility::Cross(Vec3 &p_Vector1, Vec3 &p_Vector2)
 {
 	Vec3 cross = { 0.0f };		//正規化されたベクトル
 
-	//引数チェック
-	if (NULL == p_Vector1 || NULL == p_Vector2)
-	{
-		ERROR_MESSAGE("引数エラー");
-		return cross;
-	}
-
 	//計算しやすいように代入（誤差を少なくするために[double]で計算）
-	double x1 = (double)(p_Vector1->x);
-	double y1 = (double)(p_Vector1->y);
-	double z1 = (double)(p_Vector1->z);
+	double x1 = (double)(p_Vector1.x);
+	double y1 = (double)(p_Vector1.y);
+	double z1 = (double)(p_Vector1.z);
 
-	double x2 = (double)(p_Vector2->x);
-	double y2 = (double)(p_Vector2->y);
-	double z2 = (double)(p_Vector2->z);
+	double x2 = (double)(p_Vector2.x);
+	double y2 = (double)(p_Vector2.y);
+	double z2 = (double)(p_Vector2.z);
 
 	//外積を求める
 	cross.x = (float)(y1 * z2 - z1 * y2);
@@ -184,39 +156,32 @@ Vec3 Utility::Cross(Vec3 *p_Vector1, Vec3 *p_Vector2)
 *	　正常終了		：正規化されたベクトル
 *	　以上終了		：全要素[0.0]
 *-------------------------------------------------------------------------------*/
-Vec3 Utility::Normal(Vec3 *p_Vector1, Vec3 *p_Vector2, Vec3 *p_Vector3)
+Vec3 Utility::Normal(Vec3 &p_Vector1, Vec3 &p_Vector2, Vec3 &p_Vector3)
 {
 	Vec3 normal = { 0.0f };		//正規化されたベクトル
 
-	//引数チェック
-	if (NULL == p_Vector1 || NULL == p_Vector2 || NULL == p_Vector3)
-	{
-		ERROR_MESSAGE("引数エラー");
-		return normal;
-	}
-
 	//計算しやすいように代入（誤差を少なくするために[double]で計算）
-	double x1 = (double)(p_Vector1->x);
-	double y1 = (double)(p_Vector1->y);
-	double z1 = (double)(p_Vector1->z);
+	double x1 = (double)(p_Vector1.x);
+	double y1 = (double)(p_Vector1.y);
+	double z1 = (double)(p_Vector1.z);
 
-	double x2 = (double)(p_Vector2->x);
-	double y2 = (double)(p_Vector2->y);
-	double z2 = (double)(p_Vector2->z);
+	double x2 = (double)(p_Vector2.x);
+	double y2 = (double)(p_Vector2.y);
+	double z2 = (double)(p_Vector2.z);
 
-	double x3 = (double)(p_Vector3->x);
-	double y3 = (double)(p_Vector3->y);
-	double z3 = (double)(p_Vector3->z);
+	double x3 = (double)(p_Vector3.x);
+	double y3 = (double)(p_Vector3.y);
+	double z3 = (double)(p_Vector3.z);
 
 	//ベクトル[1][2] と ベクトル[2][3] を求める
 	Vec3 tmp_normal1 = { float(x2 - x1), float(y2 - y1), float(z2 - z1) };
 	Vec3 tmp_normal2 = { float(x3 - x2), float(y3 - y2), float(z3 - z2) };
 
 	//ベクトル[1][2] と ベクトル[2][3]の外積を求める
-	normal = Cross(&tmp_normal1, &tmp_normal2);
+	normal = Cross(tmp_normal1, tmp_normal2);
 
 	//正規化して単位ベクトルにする
-	normal = Normalize(&normal);
+	normal = Normalize(normal);
 
 	return normal;
 }
